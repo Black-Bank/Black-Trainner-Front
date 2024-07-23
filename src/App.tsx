3a9f810ac810c3755bdebdfdc5f6645fa82ef713
Leo <leonardo_dwg@hotmail.com>
2024-07-23 14:33:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+import FormPaga from "./components/form";
+
+function App() {
+  return (
+    <div className="App">
+      <FormPaga />
+    </div>
+  );
+}
+
+export default App;
